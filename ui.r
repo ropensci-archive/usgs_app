bfc2ff3071e94c0c58b7de5c6a954966d1f6c354
schamberlain <myrmecocystus@gmail.com>
2013-03-30 11:14:59 -0700

    added checkbox inputs

--- ui.r
+++ ui.r
@@ -1,16 +1,35 @@
 library(shiny)
 
-# Define UI for application that plots random distributions 
 shinyUI(pageWithSidebar(
   
   headerPanel(title=HTML("usgs demo"), windowTitle="ropensci-USGS"),
   
   sidebarPanel(
     wellPanel(
+    	h4(strong("Input your taxon names:")),
       textInput(inputId="spec", label="We're using Phylomatic on the backend, so plants only for now. Enter species names, spelled correctly, separated by commas", 
                 value="Carpobrotus edulis,Rosmarinus officinalis,Ageratina riparia"),
       HTML("<br>"),
       HTML("An example query w/ more species: Bidens pilosa,Ambrosia artemisifolia,Ageratina riparia,Acroptilon repens,Ageratina adenophora,Aegilops triuncialis,Agrostis capillaris,Cinchona pubescens,Salix babylonica,Pinus caribaea")
+    ),
+    
+    wellPanel(
+    	h4(strong("ITIS options:")),
+      checkboxInput(inputId = "gethierarchyupfromtsn",
+                    label = strong("Parent taxon"),
+                    value = FALSE),
+    	
+    	checkboxInput(inputId = "gethierarchyupfromtsn",
+                    label = strong("Downstream hierarchy"),
+                    value = FALSE),
+    	
+    	checkboxInput(inputId = "gethierarchyupfromtsn",
+                    label = strong("Etc"),
+                    value = FALSE),
+    	
+    	checkboxInput(inputId = "gethierarchyupfromtsn",
+                    label = strong("Get synonyms"),
+                    value = FALSE)
     ),
     
     submitButton("Update View"),
@@ -26,9 +45,6 @@ shinyUI(pageWithSidebar(
       tabPanel("Phylogeny", plotOutput("phylogeny")),
       tabPanel("Map", plotOutput("map"))
     )
-#     h5("A phylogeny of your selected species") ,
-#     HTML("<br><br>"),
-#     tableOutput("invasiveness")
   )
   
 ))
