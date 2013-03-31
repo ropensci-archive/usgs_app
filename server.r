487197b50f4556d539578e632001eac8fb0eb5c6
schamberlain <myrmecocystus@gmail.com>
2013-03-31 12:44:56 -0700

    edits

--- server.r
+++ server.r
@@ -14,7 +14,7 @@ shinyServer(function(input, output) {
   
   datasetInput <- reactive({
 #   	conn <- taxize:::sqlite_init(path="~/ShinyApps/usgs/itis2.sqlite")
-  	conn <- taxize:::sqlite_init(path="~/github/ropensci/taxize_/inst/sql/itis2.sqlite")
+  	conn <- taxize:::sqlite_init(path="~/github/ropensci/sql/itis2.sqlite")
   	
     species <- input$spec
     species2 <- strsplit(species, ",")[[1]]
@@ -23,16 +23,17 @@ shinyServer(function(input, output) {
     if(input$locally=="local sqlite3") {locally_choice <- TRUE} else {locally_choice <- FALSE}
     
     # Get ITIS data
-    tsns <- get_tsn(searchterm=species2, searchtype="sciname", locally=locally_choice, cn=conn)
+    tsns <- na.omit(get_tsn(searchterm=species2, searchtype="sciname", locally=locally_choice, cn=conn))
+  	tsns_sp <- data.frame(sp=species2, tsn=as.vector(tsns))
     
     ## Get hierarchy up from species
     if (input$getup) {
     	if(!locally_choice){
     		registerDoMC(cores=4)
-    		itisdata_getup <- ldply(tsns, gethierarchyupfromtsn, .parallel=TRUE)	
+    		itisdata_getup <- ldply(tsns, gethierarchyupfromtsn, .parallel=TRUE)
     	} else
     	{
-    		itisdata_getup <- ldply(tsns, gethierarchyupfromtsn, locally=locally_choice)	
+    		itisdata_getup <- ldply(tsns, gethierarchyupfromtsn, locally=locally_choice, sqlconn = conn)
     	}
     } else {itisdata_getup <- NULL}
     
@@ -42,11 +43,22 @@ shinyServer(function(input, output) {
 #     	itisdata_getdown <- ldply(tsns, itis_downstream, downto = input$getdown, .parallel=TRUE)[,-1]	
 #     } else {itisdata_getdown <- NULL}
 #     
-#     ## Get synonyms
-#     if (is.character(input$getdown)) {
-#     	registerDoMC(cores=4)
-#     	itisdata_syns <- ldply(tsns, getsynonymnamesfromtsn, .parallel=TRUE)[,-1]
-#     } else {itisdata_syns <- NULL}
+    ## Get synonyms
+    if (input$getsyns) {
+    	if(!locally_choice){
+    		registerDoMC(cores=4)
+    		itisdata_syns <- ldply(tsns, getsynonymnamesfromtsn, .parallel=TRUE)[,-1]
+    	} else
+    	{
+    		getsyns <- function(x){
+					temp <- getsynonymnamesfromtsn(x, locally=locally_choice, sqlconn = conn)
+					names(temp)[1] <- "synonym"
+					data.frame(submittedName = rep(tsns_sp[tsns_sp$tsn%in%x,"sp"],nrow(temp)), temp)
+    		}
+#     		itisdata_syns <- ldply(tsns, getsynonymnamesfromtsn, locally=locally_choice, sqlconn = conn)
+    		itisdata_syns <- ldply(tsns, getsyns)
+    	}
+    } else {itisdata_syns <- NULL}
 #     
 #     ## put ITIS data into a list
 #     itisdata_list <- compact(list(itisdata_getup,itisdata_getdown,itisdata_syns))
@@ -73,30 +85,34 @@ shinyServer(function(input, output) {
     
     # Make map
     registerDoMC(cores=4)
-    out <- llply(species2, function(x) occurrencelist(x, coordinatestatus = TRUE, maxresults = 100), .parallel=TRUE)
+    out <- llply(species2, function(x) occurrencelist(x, coordinatestatus = TRUE, maxresults = 100, fixnames="changealltoorig"), .parallel=TRUE)
     map <- gbifmap(out)
     
     
     # PUt output in a list
 #     list(itisdata_list, dat, phylog2, map)
-    list(itisdata_getup, dat, phylog2, map)
+    list(itisdata_getup, itisdata_syns, dat, phylog2, map)
   })
   
   
   
-  output$itis_data <- renderTable({
+  output$itis_parent <- renderTable({
   	datasetInput()[[1]]
   })
   
-  output$invasiveness <- renderTable({
+  output$itis_syns <- renderTable({
   	datasetInput()[[2]]
   })
   
+  output$invasiveness <- renderTable({
+  	datasetInput()[[3]]
+  })
+  
   output$phylogeny <- renderPlot({
-    print(datasetInput()[[3]])
+    print(datasetInput()[[4]])
   })
   
   output$map <- renderPlot({
-    print(datasetInput()[[4]])
+    print(datasetInput()[[5]])
   })
 })
--- ui.r
+++ ui.r
@@ -10,15 +10,14 @@ shinyUI(pageWithSidebar(
       textInput(inputId="spec", label="We're using Phylomatic on the backend, so plants only for now. Enter species names, spelled correctly, separated by commas", 
                 value="Carpobrotus edulis,Rosmarinus officinalis,Ageratina riparia"),
       HTML("<br>"),
-      HTML("An example query w/ more species: Bidens pilosa,Ambrosia artemisifolia,Ageratina riparia,Acroptilon repens,Ageratina adenophora,Aegilops triuncialis,Agrostis capillaris,Cinchona pubescens,Salix babylonica,Pinus caribaea")
+      HTML("An example query w/ more species: Bidens pilosa,Ageratina riparia,Acroptilon repens,Ageratina adenophora,Aegilops triuncialis,Agrostis capillaris,Cinchona pubescens,Salix babylonica,Pinus caribaea")
     ),
     
     wellPanel(
     	h4(strong("ITIS options:")),
       checkboxInput(inputId = "getup",
                     label = strong("Parent taxon"),
-                    value = FALSE)
-    	
+                    value = TRUE),
 #     	selectInput(inputId = "getdown",
 #     							label = "Downstream hierarchy:",
 #     							choices = c("Family","Genus","Species"),
@@ -27,17 +26,16 @@ shinyUI(pageWithSidebar(
 # #     	checkboxInput(inputId = "getdown",
 # #                     label = strong("Downstream hierarchy"),
 # #                     value = FALSE),
-#     	
-#     	checkboxInput(inputId = "getsyns",
-#                     label = strong("Get synonyms"),
-#                     value = FALSE)
+    	checkboxInput(inputId = "getsyns",
+                    label = strong("Get synonyms"),
+                    value = TRUE)
     ),
     
     wellPanel(
       selectInput(inputId = "locally",
       						label = strong("Do local SQL search"),
       						choices = c("ITIS web API","local sqlite3"),
-      						selected = "ITIS web API")
+      						selected = "local sqlite3")
     ),	
     
     submitButton("Update View"),
@@ -48,7 +46,8 @@ shinyUI(pageWithSidebar(
   
   mainPanel(
     tabsetPanel(
-    	tabPanel("ITIS Data", tableOutput("itis_data")),
+    	tabPanel("ITIS Parent", tableOutput("itis_parent")),
+    	tabPanel("ITIS Synonyms", tableOutput("itis_syns")),
       tabPanel("Invasive?", tableOutput("invasiveness")),
       tabPanel("Phylogeny", plotOutput("phylogeny")),
       tabPanel("Map", plotOutput("map"))
